e168eb7f4c473baca0a30b55a8c5b283fbab6d65
cmis <cmis@cmis-hs07.(none)>
2015-08-17 11:55:21 +0700

    Created HelloWorld

---
+++ HelloWorld/HelloAge.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+public class HelloAge
+    {
+     public static void main (String[] args )
+        {
+         String input1 = JOptionPane.showInputDialog( " What is your name? " );
+         String input2 = JOptionPane.showInputDialog( " What is your age? " );
+         System.out.println( "Hello, " + input1 + " You are " + input2 + " years old." );
+         
+        } // end method main
+        
+    } // end class HelloAge
---
+++ HelloWorld/InsaneMath.java
@@ -0,0 +1,26 @@
+import javax.swing.JOptionPane;
+
+public class InsaneMath
+    {
+     public static void main (String[] args )
+        {
+         int sum1 = 0;
+         int sum2 = 0;
+         int sum3 = 0;
+         int sum4 = 0;
+         
+         String input1 = JOptionPane.showInputDialog( " What is your first number? " );
+         String input2 = JOptionPane.showInputDialog( " What is your second number? " );
+         
+         int num1 = Integer.parseInt( input1 );
+         int num2 = Integer.parseInt( input2 );
+         
+         sum1 = num1 + num2; 
+         sum2 = num2 + num1;
+         
+         System.out.println( sum1 );
+         System.out.println( sum2 );
+         
+        } // end method main
+        
+    } // end class InsaneMath
