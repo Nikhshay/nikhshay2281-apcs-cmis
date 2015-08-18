c80122bc87015990c9d6ba4f8fc464ce16162cd5
cmis <cmis@cmis-hs00.(none)>
2015-08-18 09:56:21 +0700

    Done with InsaneMath

--- InsaneMath/InsaneMath.java
+++ InsaneMath/InsaneMath.java
@@ -10,14 +10,18 @@ public class InsaneMath
          int nsum4 = 0;
          int product1 = 0;
          int product2 = 0;
-         int quotion1 = 0;
-         int quotion2 = 0;
+         float quotion1 = 0;
+         float quotion2 = 0;
+         int modulo1 = 0;
+         int modulo2 = 0;
          
          String input1 = JOptionPane.showInputDialog( " What is your first number? " );
          String input2 = JOptionPane.showInputDialog( " What is your second number? " );
          
          int num1 = Integer.parseInt( input1 );
          int num2 = Integer.parseInt( input2 );
+         float num3 = Integer.parseInt( input1 );
+         float num4 = Integer.parseInt( input2 );
          
          psum1 = num1 + num2; 
          psum2 = num2 + num1;
@@ -25,8 +29,10 @@ public class InsaneMath
          nsum4 = num2 - num1;
          product1 = num1 * num2;
          product2 = num2 * num1;
-         quotion1 = num1 / num2;
-         quotion2 = num2 / num1;
+         quotion1 = num3 / num4;
+         quotion2 = num4 / num3;
+         modulo1 = num1 % num2;
+         modulo2 = num2 % num1;
          
          System.out.println( num1 + " + " + num2 + " = " + psum1 );
          System.out.println( num2 + " + " + num1 + " = " + psum2 );
@@ -34,8 +40,10 @@ public class InsaneMath
          System.out.println( num2 + " - " + num1 + " = " + nsum4 );
          System.out.println( num1 + " * " + num2 + " = " + product1 );
          System.out.println( num2 + " * " + num1 + " = " + product2 );
-         System.out.println( num1 + " / " + num2 + " = " + quotion1 );
-         System.out.println( num2 + " / " + num1 + " = " + quotion2 );         
+         System.out.println( num3 + " / " + num4 + " = " + quotion1 );
+         System.out.println( num4 + " / " + num3 + " = " + quotion2 );         
+         System.out.println( num1 + " / " + num2 + " = " + modulo1 );
+         System.out.println( num2 + " / " + num1 + " = " + modulo2 ); 
          
         } // end method main
         
