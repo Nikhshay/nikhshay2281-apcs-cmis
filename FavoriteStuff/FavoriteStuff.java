3418c70be75ce6254b54c14a3bd73d3f9bc582e3
cmis <cmis@cmis-hs07.(none)>
2015-08-19 11:57:05 +0700

    Made FavoriteStuff

---
+++ FavoriteStuff/FavoriteStuff.java
@@ -0,0 +1,19 @@
+package FavoriteStuff;
+
+public class FavoriteStuff
+    {
+     public static void main (String args[] )
+        {
+         String input1 = JOptionPane.showInputDialog( " What is your favorite food? " );
+         String input2 = JOptionPane.showInputDialog( " What is your favorite color? " );
+         String input3 = JOptionPane.showInputDialog( " What is your favorite movie? " );
+         String input4 = JOptionPane.showInputDialog( " What is your favorite musical group? " );
+     
+         
+         System.out.println( "Your favorite food is " + input1 );
+         System.out.println( "Your favorite color is " + input2 );
+         System.out.println( "Your favorite movie is " + input3 );
+         System.out.println( "Your favorite musical group is " + input4 );
+        } // end method main
+        
+    }// end class FavoriteStuff
