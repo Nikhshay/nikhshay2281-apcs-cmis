ffa5fc243e3d55b354a0c58c65a033a914372289
cmis <cmis@cmis-hs07.(none)>
2015-09-23 11:47:16 +0700

    didnt get that far

--- WeatherChaos.java
+++ WeatherChaos.java
@@ -7,21 +7,44 @@ public class WeatherChaos
         String input = JOptionPane.showInputDialog("please give me a number");
         int num1 = Integer.parseInt(input);
         int randomint[] = new int[num1];
-        int days = 0;
+        int swing;
+        int days = 1;
+        int min = 99;
+        int max = 0;
+        int sum = 0;
         
         for ( int index = 0; index < randomint.length; index++ )
         {
             randomint[index] = (int) (Math.random() * 200) - 100;
+            if (randomint[index] < min)
+            {
+                min = randomint[index];
+            }
+            
+            if (randomint[index] > max)
+            {
+                max = randomint[index];
+            }
+            sum += randomint[index];
         } // end for loop
         if (num1 > 0 && num1 < 31)
         {
             System.out.print("Days \t" + "temperature \t" + "temperature swing from the previous day \t" + "description");
-            days = days + 1;
+
             for (int i = 0; i < randomint.length; i++)
             { 
-                System.out.println(days + "\t" + randomint[i] + "\t" + randomint[i-1] + "");
+               days += 1;
+               swing = randomint[i-1]; 
+               System.out.println(days + "\t" + randomint[i] + "\t" + randomint[swing] + "wait");
             }
+            
+            
+            
+            System.out.println("MIN: " + min);
+            System.out.println("MAX: " + max);
+            System.out.println("AVG: " + (double) sum / (double) num1);
         }
+        
         else
         {
             System.out.println("That's not a valid number of days!");
--- dsg.java
+++ dsg.java
@@ -1,36 +1,73 @@
 import javax.swing.JOptionPane;
-
-
-    import javax.swing.JOptionPane;
-public class dsg
+public class Histogram 
+{ 
+    public static void main ( String args[] )
     {
-     public static void main (String[] args )
+        String input1 = JOptionPane.showInputDialog( "Enter a number.");
+        int num= Integer.parseInt( input1);
+        int randomNums[] = new int [num];
+        int counter [] = new int [100];
+        for( int index = 0; index < randomNums.length; index++ ) 
         {
-                String input1 = JOptionPane.showInputDialog( " What is your number? " );
-         
-        int num1 = Integer.parseInt( input1 );
-         
-        double[] myArray = new double[num1];
-        for( int index = 0; index < myArray.length; index++ )
+            randomNums[index] = (int)((Math.random() * 100)); // producing random numbers
+            for( int index1 = 0; index1 < 100; index1 ++)
+            {  
+                if (randomNums[index] == index1)
+                {
+                    counter[index1]++;
+                }//end if statement
+            } //end for loop    
+        } // end for loop
+        for( int index = 0; index < 100; index++ )// producing number of hashtag according to counts
         {
-            myArray[index] = (double) (Math.random() * 51); 
-            System.out.println("["+index+"]" + myArray[index]);
-            }// end making decimal number // end making decimal number 
-       
-        int index2 = myArray.length-1;
-        
-        while (index2 >= 0)
-           {
-            System.out.println("["+index2+"]" + myArray[index2] );
-            index2--;
-             
-            int b = 0;
-            
-            for (int a = (int)myArray[index2]; a > 0; a++ )
+            System.out.print(index + " : ");
+            for( int hashtag = 0; hashtag < counter[index]; hashtag++)
             {
-                b++;
-                System.out.println(b);
-            }
-        }
-    }
-}
+                System.out.print("#"); 
+            } // end for loop
+            System.out.println(" ");
+        } // end for loop
+
+        int minimumNum = 100;
+        for( int index = 0; index < randomNums.length; index++ ) // finding minimum number
+        {
+            if(randomNums[index] < minimumNum)
+            {
+                minimumNum = randomNums[index];
+            }// end if statement
+        } // end for loop
+        System.out.println("MIN: " + minimumNum);
+        
+        int maximumNum = 0;
+        for( int index = 0; index < randomNums.length; index++ ) // finding maximum number
+        {
+            if(randomNums[index] > maximumNum)
+            {
+                maximumNum = randomNums[index];
+            }// end if statement
+        } // end for loop
+        System.out.println("MAX: " + maximumNum);
+
+        int sum = 0; 
+        for( int index = 0; index < randomNums.length; index++ ) // finding the sum of all random numbers
+        {
+            sum = randomNums[index] + sum ;
+        } // end for loop
+        System.out.println("SUM: " + sum);
+
+        double average = 0; // finding the average of all random numbers
+        average = (double)(sum) / randomNums.length ;
+        System.out.println("AVG: " + average);
+        
+        int mostOc = 0;
+        for( int index = 0; index < counter.length; index++ ) // finding the random number with most occurances
+        {
+            if(counter[index] >= mostOc)
+            {
+                mostOc = index;
+               
+            }// end if statement
+        } // end for loop
+        System.out.println("MST: " + mostOc);
+    } // end main
+} // end Histogram
