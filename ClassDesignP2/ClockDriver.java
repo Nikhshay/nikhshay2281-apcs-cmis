c34b7aa3e52bc1f220a00b8592ca1711fc2d12a2
cmis <cmis@cmis-hs07.(none)>
2015-10-05 11:57:36 +0700

    lol

--- ClassDesignP2/ClockDriver.java
+++ ClassDesignP2/ClockDriver.java
@@ -1,12 +1,17 @@
 package ClassDesignP2;
+import javax.swing.JOptionPane; 
 
 public class ClockDriver
 {
     public static void main (String[] arg)
     {
+       int hour = Integer.parseInt(JOptionPane.showInputDialog("What the hour?"));
+       int min = Integer.parseInt(JOptionPane.showInputDialog("What the minute?"));
+       int sec = Integer.parseInt(JOptionPane.showInputDialog("What the seconds?"));
+
         Clock bedroomClock = new Clock();
         
-        Clock kitchenClock = new Clock( 24 , 00 , 00 );
+        Clock kitchenClock = new Clock( hour , min , sec );
         
         System.out.println(bedroomClock);
         System.out.println(kitchenClock);
