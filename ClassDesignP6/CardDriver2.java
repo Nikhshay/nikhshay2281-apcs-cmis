657ad21cc1509ff9a2be2b3103b2d04dfdc3d1b5
cmis <cmis@cmis-hs07.(none)>
2015-11-05 14:35:45 +0700

    i m doomed

--- ClassDesignP6/CardDriver2.java
+++ ClassDesignP6/CardDriver2.java
@@ -4,27 +4,79 @@ public class CardDriver2
 {
     public static void main (String[] arg)
     {
-     String[] allRank = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
-     int[] point = {2, 3, 4, 5, 6, 7, 8, 9, 10, 10 , 10, 10, 11};
-     String[] allSuit = {"clubs", "spades", "diamonds", "hearts"};
-     
-     Card[] Dealer = new Card [2];
-     
-     System.out.print("Dealer: ");
-     for( int i = 0; i < Dealer.length; i++)
-       {
-        int a = ((int) (Math.random()*13));
-        int b = ((int) (Math.random()*4));
-        Dealer[i] = new Card( allRank[a], allSuit[b] );
-        System.out.print(Dealer[i] + " ");
+        int Worth = 0;
+        int TotalWorth = 0;
         
-        for(int c = 0; c < allRank.length; c++)
-        {
-            allRank[c] = Integer.parseInt(allRank[c]);
-            System.out.print(allRank[c]);
+        String[] allRank = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+        String[] allSuit = {"clubs", "spades", "diamonds", "hearts"};
+     
+        Card[] Dealer = new Card [2];
+        
+       for( int z = 1; z < 2; z++)
+       {
+       for( int i = 0; i < Dealer.length; i++)
+       {
+          int a = ((int) (Math.random()*13));
+          int b = ((int) (Math.random()*4));
+          Dealer[i] = new Card( allRank[a], allSuit[b] );         
+          System.out.print("Dealer card ");
+          System.out.print( i+1 );
+          System.out.print(" : " + Dealer[i] + "\n");
+
+           if (allRank[a]  == ("A") )
+             {
+                Worth = 11;
+            }
+            else if (allRank[a]  == ("2") )
+             {
+                Worth = 2;
+             }   
+            else if (allRank[a]  == ("3") )
+             {
+                Worth = 3;
+             }   
+           else if (allRank[a]  == ("4") )
+             {
+                Worth = 4;
+             }    
+           else if (allRank[a]  == ("5") )
+             {
+                Worth = 5;
+             }   
+            else if (allRank[a]  == ("6") )
+             {
+                Worth = 6;
+             }   
+           else if (allRank[a]  == ("7") )
+             {
+                Worth = 7;
+             }       
+             else if (allRank[a]  == ("8") )
+             {
+                Worth = 8;
+             }   
+            else if (allRank[a]  == ("9") )
+             {
+                Worth = 9;
+             }   
+           else if (allRank[a]  == ("10") )
+             {
+                Worth = 10;
+             }     
+           else if (allRank[a]  == ("J") || allRank[a]  ==("Q") || allRank[a]  ==("K") )
+             {
+                Worth = 10;
+            }
+           TotalWorth += Worth;
+           if (TotalWorth == 21)
+           {
+               System.out.println( "You are the winner!)"); 
+            }
+        }
+       System.out.println( "\n" + "Points in hand " + TotalWorth + "\n");
+      }
+            
+           
         
         }
-
     }
-  }
-}
