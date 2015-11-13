ac9f7deb217bcdf844359e3d874799598ca9a36e
cmis <cmis@cmis-hs07.(none)>
2015-11-13 09:56:27 +0700

    failed as usual

---
+++ Hand.java
@@ -0,0 +1,11 @@
+public class Hand
+    {
+     public static void main (String args[] )
+        {
+          int numberofcards = 2;
+          int totalpoints;
+          
+          for (
+        } 
+        
+    }
