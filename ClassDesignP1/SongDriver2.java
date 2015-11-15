de1ed34a9385ce6c639619b2ef5bbdcc216e1dfe
nikhshay2281-cmis <nikhshay2281@cmis.ac.th>
2015-11-15 21:03:09 +0700

    continue working

--- ClassDesignP1/SongDriver2.java
+++ ClassDesignP1/SongDriver2.java
@@ -1,17 +1,28 @@
 package ClassDesignP1;
 
+import javax.swing.JOptionPane;
+
 public class SongDriver2
 {
     public static void main (String[] arg)
     {
+        String input1 = JOptionPane.showInputDialog( " What is your username? " );
+        String input2 = JOptionPane.showInputDialog( " What is your password? " );
+        int num1 = Integer.parseInt( input2 );
+        
+        
+        
         Song1 SongA = new Song1();
         Song1 SongB = new Song1( "Stay with me", "chill", 214);
         
         System.out.println(SongA);
         System.out.println(SongB);
         
-        SongA.getYearReleased();
-        SongA.setYearReleased(2019);
+        if (input1.equals ("Nikhshay") && num1 == 2281 ) 
+        {
+            SongA.getYearReleased();
+            SongA.setYearReleased(2019);;
+        }
         System.out.println(SongA);
     }
 }
