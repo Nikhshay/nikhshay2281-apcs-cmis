a62f748d264594e6aa007825d54e743d089282c2
nikhshay2281-cmis <nikhshay2281@cmis.ac.th>
2016-02-01 21:08:45 +0700

    worked on a little

--- Array2D2/Array2D2.java
+++ Array2D2/Array2D2.java
@@ -4,15 +4,30 @@ public class Array2D2
 {
     public static void main ( String arg[] )
     {
-        int number[][] = new int[3][4];
+        int[][] number = new int[3][5];
+        int i = 0;
         for(int row = 0; row < number.length; row++)
-        {
+        {      
             for(int col = 0; col < number[0].length; col++)
              {
-                number[row][col] = (int)(Math.random()*10); 
-                System.out.print(number[row][col] + "    "); 
+                number[row][col] = i + col + 1 ; 
              }
-            System.out.print("\n"); 
+            i = i + 5;
         }
+        
+        int total =  0;
+        for (int[] x : number )
+        {
+            for(int num : x)
+             {
+                 total += num;
+                 System.out.print( num  + "\t");
+              }
+            System.out.println();
+        }
+        
+        System.out.print("The sum total of all 15 elements of the array is : " + total); //120
+        System.out.print("The sum total of each row of the array is : ");
+        System.out.print("The sum total of each column of the array is : ");
     }
 }
