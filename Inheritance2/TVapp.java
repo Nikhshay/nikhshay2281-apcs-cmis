90adde2c7606adf622eb1e22e92a19aa71ca8776
cmis <cmis@cmis-hs08.(none)>
2016-02-24 09:18:13 +0700

    done in2

--- Inheritance2/TVapp.java
+++ Inheritance2/TVapp.java
@@ -5,15 +5,15 @@ public class TVapp
     public static void main(String[] args)
     {
         Television[] tvs = new Television[5];
-        tvs[0] = new Television("HD Bronze", 59.99);
-        tvs[1] = new Plasma("Plasma Platinum", 299.99);
-        tvs[2] = new LED("LED low", 19.99);
-        tvs[3] = new DLP("DLP Silver", 99.99);
-        tvs[4] = new LCD("LED GOld", 199.99);
+        tvs[0] = new Television("HD Bronze", 25.00);
+        tvs[1] = new DLP("DLP Silver", 59.99);
+        tvs[2] = new LED("LED Gold", 99.99);
+        tvs[3] = new LCD("LCD Platinum", 199.99);
+        tvs[4] = new Plasma("Dayum Plasma", 199999.99);
     
         for ( Television tv : tvs )
         {
-            System.out.println(tv + "\n");
+            System.out.println(tv);
         }     
     }
 }
