5e7bb89fc929cad0a1b7a1b3160785efad2e5ff1
cmis <cmis@cmis-hs09.(none)>
2016-03-16 14:37:02 +0700

    started greenfoot

---
+++ GreenFoot/Paddle.java
@@ -0,0 +1,27 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Paddle here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Paddle extends Actor
+{
+    /**
+     * Act - do whatever the Paddle wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        if (Greenfoot.isKeyDown ("right"))
+         {
+         move(2);
+        }
+        
+        if (Greenfoot.isKeyDown ("left"))
+         {
+         move(-2);
+        }
+    }    
+}
